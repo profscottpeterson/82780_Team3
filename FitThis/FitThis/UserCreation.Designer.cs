2e619a4122fb9a672eb05e207d3644528ab2e1be
Jenny Druckrey <jenny.druckrey@mymail.nwtc.edu>
2018-10-14 12:20:17 -0500

    User creation form added.

---
+++ FitThis/FitThis/UserCreation.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace FitThis
+{
+    partial class UserCreationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ComboBox activityComboBox;
+            this.UCTitleLabel = new System.Windows.Forms.Label();
+            this.fNameTxtBox = new System.Windows.Forms.TextBox();
+            this.fNameLabel = new System.Windows.Forms.Label();
+            this.lNameLabel = new System.Windows.Forms.Label();
+            this.lNameTxtBox = new System.Windows.Forms.TextBox();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.ageTxtBox = new System.Windows.Forms.TextBox();
+            this.weightLabel = new System.Windows.Forms.Label();
+            this.weightTxtBox = new System.Windows.Forms.TextBox();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.heigtFtTxtBox = new System.Windows.Forms.TextBox();
+            this.ftLabel = new System.Windows.Forms.Label();
+            this.inchesLabel = new System.Windows.Forms.Label();
+            this.HeightInchTxtBox = new System.Windows.Forms.TextBox();
+            this.maleRadioButton = new System.Windows.Forms.RadioButton();
+            this.FeamlerRadioButton = new System.Windows.Forms.RadioButton();
+            this.genderLabel = new System.Windows.Forms.Label();
+            this.activityLabel = new System.Windows.Forms.Label();
+            this.goalWeightLabel = new System.Windows.Forms.Label();
+            this.goalWeightTxtBox = new System.Windows.Forms.TextBox();
+            this.createProfileButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            activityComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // UCTitleLabel
+            // 
+            this.UCTitleLabel.AutoSize = true;
+            this.UCTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UCTitleLabel.Location = new System.Drawing.Point(165, 35);
+            this.UCTitleLabel.Name = "UCTitleLabel";
+            this.UCTitleLabel.Size = new System.Drawing.Size(167, 24);
+            this.UCTitleLabel.TabIndex = 0;
+            this.UCTitleLabel.Text = "Create Your Profile";
+            // 
+            // fNameTxtBox
+            // 
+            this.fNameTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fNameTxtBox.Location = new System.Drawing.Point(157, 85);
+            this.fNameTxtBox.Name = "fNameTxtBox";
+            this.fNameTxtBox.Size = new System.Drawing.Size(100, 22);
+            this.fNameTxtBox.TabIndex = 1;
+            // 
+            // fNameLabel
+            // 
+            this.fNameLabel.AutoSize = true;
+            this.fNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fNameLabel.Location = new System.Drawing.Point(75, 89);
+            this.fNameLabel.Name = "fNameLabel";
+            this.fNameLabel.Size = new System.Drawing.Size(76, 16);
+            this.fNameLabel.TabIndex = 2;
+            this.fNameLabel.Text = "First Name:";
+            // 
+            // lNameLabel
+            // 
+            this.lNameLabel.AutoSize = true;
+            this.lNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNameLabel.Location = new System.Drawing.Point(75, 128);
+            this.lNameLabel.Name = "lNameLabel";
+            this.lNameLabel.Size = new System.Drawing.Size(76, 16);
+            this.lNameLabel.TabIndex = 4;
+            this.lNameLabel.Text = "Last Name:";
+            // 
+            // lNameTxtBox
+            // 
+            this.lNameTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNameTxtBox.Location = new System.Drawing.Point(157, 124);
+            this.lNameTxtBox.Name = "lNameTxtBox";
+            this.lNameTxtBox.Size = new System.Drawing.Size(100, 22);
+            this.lNameTxtBox.TabIndex = 3;
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageLabel.Location = new System.Drawing.Point(59, 167);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(91, 16);
+            this.ageLabel.TabIndex = 6;
+            this.ageLabel.Text = "Age (In years)";
+            // 
+            // ageTxtBox
+            // 
+            this.ageTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageTxtBox.Location = new System.Drawing.Point(157, 164);
+            this.ageTxtBox.Name = "ageTxtBox";
+            this.ageTxtBox.Size = new System.Drawing.Size(100, 22);
+            this.ageTxtBox.TabIndex = 5;
+            // 
+            // weightLabel
+            // 
+            this.weightLabel.AutoSize = true;
+            this.weightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightLabel.Location = new System.Drawing.Point(38, 208);
+            this.weightLabel.Name = "weightLabel";
+            this.weightLabel.Size = new System.Drawing.Size(113, 16);
+            this.weightLabel.TabIndex = 8;
+            this.weightLabel.Text = "Weight (In Ponds)";
+            // 
+            // weightTxtBox
+            // 
+            this.weightTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightTxtBox.Location = new System.Drawing.Point(157, 205);
+            this.weightTxtBox.Name = "weightTxtBox";
+            this.weightTxtBox.Size = new System.Drawing.Size(100, 22);
+            this.weightTxtBox.TabIndex = 7;
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heightLabel.Location = new System.Drawing.Point(86, 250);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(47, 16);
+            this.heightLabel.TabIndex = 10;
+            this.heightLabel.Text = "Height";
+            // 
+            // heigtFtTxtBox
+            // 
+            this.heigtFtTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heigtFtTxtBox.Location = new System.Drawing.Point(157, 246);
+            this.heigtFtTxtBox.Name = "heigtFtTxtBox";
+            this.heigtFtTxtBox.Size = new System.Drawing.Size(44, 22);
+            this.heigtFtTxtBox.TabIndex = 9;
+            // 
+            // ftLabel
+            // 
+            this.ftLabel.AutoSize = true;
+            this.ftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ftLabel.Location = new System.Drawing.Point(207, 250);
+            this.ftLabel.Name = "ftLabel";
+            this.ftLabel.Size = new System.Drawing.Size(19, 16);
+            this.ftLabel.TabIndex = 11;
+            this.ftLabel.Text = "Ft";
+            // 
+            // inchesLabel
+            // 
+            this.inchesLabel.AutoSize = true;
+            this.inchesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inchesLabel.Location = new System.Drawing.Point(282, 250);
+            this.inchesLabel.Name = "inchesLabel";
+            this.inchesLabel.Size = new System.Drawing.Size(47, 16);
+            this.inchesLabel.TabIndex = 13;
+            this.inchesLabel.Text = "Inches";
+            // 
+            // HeightInchTxtBox
+            // 
+            this.HeightInchTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeightInchTxtBox.Location = new System.Drawing.Point(232, 246);
+            this.HeightInchTxtBox.Name = "HeightInchTxtBox";
+            this.HeightInchTxtBox.Size = new System.Drawing.Size(44, 22);
+            this.HeightInchTxtBox.TabIndex = 12;
+            // 
+            // maleRadioButton
+            // 
+            this.maleRadioButton.AutoSize = true;
+            this.maleRadioButton.Checked = true;
+            this.maleRadioButton.Location = new System.Drawing.Point(155, 294);
+            this.maleRadioButton.Name = "maleRadioButton";
+            this.maleRadioButton.Size = new System.Drawing.Size(48, 17);
+            this.maleRadioButton.TabIndex = 14;
+            this.maleRadioButton.TabStop = true;
+            this.maleRadioButton.Text = "Male";
+            this.maleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // FeamlerRadioButton
+            // 
+            this.FeamlerRadioButton.AutoSize = true;
+            this.FeamlerRadioButton.Location = new System.Drawing.Point(217, 294);
+            this.FeamlerRadioButton.Name = "FeamlerRadioButton";
+            this.FeamlerRadioButton.Size = new System.Drawing.Size(59, 17);
+            this.FeamlerRadioButton.TabIndex = 15;
+            this.FeamlerRadioButton.Text = "Female";
+            this.FeamlerRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // genderLabel
+            // 
+            this.genderLabel.AutoSize = true;
+            this.genderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genderLabel.Location = new System.Drawing.Point(86, 294);
+            this.genderLabel.Name = "genderLabel";
+            this.genderLabel.Size = new System.Drawing.Size(53, 16);
+            this.genderLabel.TabIndex = 16;
+            this.genderLabel.Text = "Gender";
+            // 
+            // activityLabel
+            // 
+            this.activityLabel.AutoSize = true;
+            this.activityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.activityLabel.Location = new System.Drawing.Point(20, 332);
+            this.activityLabel.Name = "activityLabel";
+            this.activityLabel.Size = new System.Drawing.Size(131, 16);
+            this.activityLabel.TabIndex = 18;
+            this.activityLabel.Text = "Current Activity Level";
+            // 
+            // goalWeightLabel
+            // 
+            this.goalWeightLabel.AutoSize = true;
+            this.goalWeightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goalWeightLabel.Location = new System.Drawing.Point(20, 368);
+            this.goalWeightLabel.Name = "goalWeightLabel";
+            this.goalWeightLabel.Size = new System.Drawing.Size(85, 16);
+            this.goalWeightLabel.TabIndex = 20;
+            this.goalWeightLabel.Text = "Goal Weight:";
+            // 
+            // goalWeightTxtBox
+            // 
+            this.goalWeightTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goalWeightTxtBox.Location = new System.Drawing.Point(157, 365);
+            this.goalWeightTxtBox.Name = "goalWeightTxtBox";
+            this.goalWeightTxtBox.Size = new System.Drawing.Size(100, 22);
+            this.goalWeightTxtBox.TabIndex = 19;
+            // 
+            // activityComboBox
+            // 
+            activityComboBox.AllowDrop = true;
+            activityComboBox.AutoCompleteCustomSource.AddRange(new string[] {
+            "\"Sedentary\"",
+            "\"Lightly Active\"",
+            "\"Moderatly Active\"",
+            "\"Highly Active\""});
+            activityComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            activityComboBox.FormattingEnabled = true;
+            activityComboBox.Items.AddRange(new object[] {
+            "\"Sedentary\"",
+            "\"Lightly Active\"",
+            "\"Moderately Active\"",
+            "\"Very Active\"",
+            "\"Extra Active\""});
+            activityComboBox.Location = new System.Drawing.Point(158, 326);
+            activityComboBox.Name = "activityComboBox";
+            activityComboBox.Size = new System.Drawing.Size(121, 24);
+            activityComboBox.TabIndex = 21;
+            // 
+            // createProfileButton
+            // 
+            this.createProfileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createProfileButton.Location = new System.Drawing.Point(129, 420);
+            this.createProfileButton.Name = "createProfileButton";
+            this.createProfileButton.Size = new System.Drawing.Size(108, 23);
+            this.createProfileButton.TabIndex = 22;
+            this.createProfileButton.Text = "Create Profile";
+            this.createProfileButton.UseVisualStyleBackColor = true;
+            this.createProfileButton.Click += new System.EventHandler(this.createProfileButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(253, 420);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(108, 23);
+            this.cancelButton.TabIndex = 23;
+            this.cancelButton.Text = "Cancel Button";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // UserCreationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 455);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.createProfileButton);
+            this.Controls.Add(activityComboBox);
+            this.Controls.Add(this.goalWeightLabel);
+            this.Controls.Add(this.goalWeightTxtBox);
+            this.Controls.Add(this.activityLabel);
+            this.Controls.Add(this.genderLabel);
+            this.Controls.Add(this.FeamlerRadioButton);
+            this.Controls.Add(this.maleRadioButton);
+            this.Controls.Add(this.inchesLabel);
+            this.Controls.Add(this.HeightInchTxtBox);
+            this.Controls.Add(this.ftLabel);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.heigtFtTxtBox);
+            this.Controls.Add(this.weightLabel);
+            this.Controls.Add(this.weightTxtBox);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.ageTxtBox);
+            this.Controls.Add(this.lNameLabel);
+            this.Controls.Add(this.lNameTxtBox);
+            this.Controls.Add(this.fNameLabel);
+            this.Controls.Add(this.fNameTxtBox);
+            this.Controls.Add(this.UCTitleLabel);
+            this.Name = "UserCreationForm";
+            this.Text = "User Creation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UCTitleLabel;
+        private System.Windows.Forms.TextBox fNameTxtBox;
+        private System.Windows.Forms.Label fNameLabel;
+        private System.Windows.Forms.Label lNameLabel;
+        private System.Windows.Forms.TextBox lNameTxtBox;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.TextBox ageTxtBox;
+        private System.Windows.Forms.Label weightLabel;
+        private System.Windows.Forms.TextBox weightTxtBox;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.TextBox heigtFtTxtBox;
+        private System.Windows.Forms.Label ftLabel;
+        private System.Windows.Forms.Label inchesLabel;
+        private System.Windows.Forms.TextBox HeightInchTxtBox;
+        private System.Windows.Forms.RadioButton maleRadioButton;
+        private System.Windows.Forms.RadioButton FeamlerRadioButton;
+        private System.Windows.Forms.Label genderLabel;
+        private System.Windows.Forms.Label activityLabel;
+        private System.Windows.Forms.Label goalWeightLabel;
+        private System.Windows.Forms.TextBox goalWeightTxtBox;
+        private System.Windows.Forms.Button createProfileButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ FitThis/FitThis/UserCreation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FitThis
+{
+    public partial class UserCreationForm : Form
+    {
+        public UserCreationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void createProfileButton_Click(object sender, EventArgs e)
+        {
+            // Need to do data validation & pass values onto User class
+
+            string ucfName;
+            string ucLName;
+            int ucAge;
+            int ucHeight;
+            int ucWeight;
+            string ucGender;
+            string ucActivityLevel;
+            int ucGoalWeight;
+        }
+    }
+}
