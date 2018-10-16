c6aa45b4b405049c144920288a6ecbf1ceea6a3f
Jacob Rose <jacob.rose45@outlook.com>
2018-10-16 14:51:45 -0500

    Created the activity dummy data and interface

--- FitThis/FitThis/FitThisHUB.cs
+++ FitThis/FitThis/FitThisHUB.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FitThis.Classes;
+
+
 
 namespace FitThis
 {
@@ -15,6 +18,44 @@ namespace FitThis
         public FitThisHUB()
         {
             InitializeComponent();
+            
+        }
+
+        private void btnAddActivity_Click(object sender, EventArgs e)
+        {
+            Activity active = new Activity();
+            string act = "";
+            int duration = 0;
+
+            if(combActivities.SelectedIndex == -1)
+            {
+                MessageBox.Show("YOu MUST SELECT A WORKOUT!!!!!!!!!!!!");
+                combActivities.Focus();
+            }
+            else
+            {
+                act = combActivities.GetItemText(this.combActivities.SelectedItem);
+                MessageBox.Show(act);
+
+                if (tbxDuration.Text.All(char.IsDigit))
+                {
+                    Int32.TryParse(tbxDuration.Text, out duration);
+                }
+                int cals = active.CaloriesPerHour(act);
+
+                double total;
+                total = active.CaloriesBurned(duration, cals);
+                lblCaloriesBurnedDisplay.Text = total.ToString();
+            }
+
+            
+
+        }
+
+        private void btnClearActivity_Click(object sender, EventArgs e)
+        {
+            combActivities.SelectedIndex = -1;
+            tbxDuration.Clear();
         }
     }
 }
