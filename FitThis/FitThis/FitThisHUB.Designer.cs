dde2f035272589f5d414cf098eb5a96b85a1b812
Jacob Rose <jacob.rose45@outlook.com>
2018-10-30 21:37:02 -0500

    removed test tab

--- FitThis/FitThis/FitThisHUB.Designer.cs
+++ FitThis/FitThis/FitThisHUB.Designer.cs
@@ -52,22 +52,11 @@
             this.combActivities = new System.Windows.Forms.ComboBox();
             this.btnClearActivity = new System.Windows.Forms.Button();
             this.btnAddActivity = new System.Windows.Forms.Button();
-            this.SQLTest = new System.Windows.Forms.TabPage();
-            this.btnViewFood = new System.Windows.Forms.Button();
-            this.btnViewUser = new System.Windows.Forms.Button();
-            this.btnInsertData = new System.Windows.Forms.Button();
-            this.btnCreateTables = new System.Windows.Forms.Button();
-            this.btnInsertTestData = new System.Windows.Forms.Button();
-            this.txbResults = new System.Windows.Forms.TextBox();
-            this.btnViewData = new System.Windows.Forms.Button();
-            this.btnCreateTestTable = new System.Windows.Forms.Button();
-            this.CreateConnectDb = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.tabConsole1.SuspendLayout();
             this.tabPersonal.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tabActivity.SuspendLayout();
-            this.SQLTest.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabConsole1
@@ -77,7 +66,6 @@
             this.tabConsole1.Controls.Add(this.tabFood);
             this.tabConsole1.Controls.Add(this.tabPersonal);
             this.tabConsole1.Controls.Add(this.tabActivity);
-            this.tabConsole1.Controls.Add(this.SQLTest);
             this.tabConsole1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabConsole1.Location = new System.Drawing.Point(196, 12);
             this.tabConsole1.Name = "tabConsole1";
@@ -88,28 +76,28 @@
             // tabDash
             // 
             this.tabDash.BackColor = System.Drawing.Color.Lime;
-            this.tabDash.Location = new System.Drawing.Point(4, 35);
+            this.tabDash.Location = new System.Drawing.Point(4, 27);
             this.tabDash.Name = "tabDash";
             this.tabDash.Padding = new System.Windows.Forms.Padding(3);
-            this.tabDash.Size = new System.Drawing.Size(792, 572);
+            this.tabDash.Size = new System.Drawing.Size(792, 580);
             this.tabDash.TabIndex = 4;
             this.tabDash.Text = "Dashboard";
             // 
             // tabWeight
             // 
             this.tabWeight.BackColor = System.Drawing.Color.Lime;
-            this.tabWeight.Location = new System.Drawing.Point(4, 35);
+            this.tabWeight.Location = new System.Drawing.Point(4, 27);
             this.tabWeight.Name = "tabWeight";
-            this.tabWeight.Size = new System.Drawing.Size(792, 572);
+            this.tabWeight.Size = new System.Drawing.Size(792, 580);
             this.tabWeight.TabIndex = 2;
             this.tabWeight.Text = "Weight Log";
             // 
             // tabFood
             // 
             this.tabFood.BackColor = System.Drawing.Color.Lime;
-            this.tabFood.Location = new System.Drawing.Point(4, 35);
+            this.tabFood.Location = new System.Drawing.Point(4, 27);
             this.tabFood.Name = "tabFood";
-            this.tabFood.Size = new System.Drawing.Size(792, 572);
+            this.tabFood.Size = new System.Drawing.Size(792, 580);
             this.tabFood.TabIndex = 3;
             this.tabFood.Text = "Food Log";
             // 
@@ -126,10 +114,10 @@
             this.tabPersonal.Controls.Add(this.txtActLvl);
             this.tabPersonal.Controls.Add(this.lblName);
             this.tabPersonal.Controls.Add(this.txtName);
-            this.tabPersonal.Location = new System.Drawing.Point(4, 35);
+            this.tabPersonal.Location = new System.Drawing.Point(4, 27);
             this.tabPersonal.Name = "tabPersonal";
             this.tabPersonal.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPersonal.Size = new System.Drawing.Size(792, 572);
+            this.tabPersonal.Size = new System.Drawing.Size(792, 580);
             this.tabPersonal.TabIndex = 0;
             this.tabPersonal.Text = "Personal Information";
             // 
@@ -147,7 +135,7 @@
             this.txtStrtWght.Enabled = false;
             this.txtStrtWght.Location = new System.Drawing.Point(227, 105);
             this.txtStrtWght.Name = "txtStrtWght";
-            this.txtStrtWght.Size = new System.Drawing.Size(100, 30);
+            this.txtStrtWght.Size = new System.Drawing.Size(100, 23);
             this.txtStrtWght.TabIndex = 7;
             // 
             // lblStrtWght
@@ -156,7 +144,7 @@
             this.lblStrtWght.BackColor = System.Drawing.Color.Orange;
             this.lblStrtWght.Location = new System.Drawing.Point(18, 108);
             this.lblStrtWght.Name = "lblStrtWght";
-            this.lblStrtWght.Size = new System.Drawing.Size(165, 26);
+            this.lblStrtWght.Size = new System.Drawing.Size(109, 18);
             this.lblStrtWght.TabIndex = 6;
             this.lblStrtWght.Text = "Starting Weight:";
             // 
@@ -166,7 +154,7 @@
             this.lblHeight.BackColor = System.Drawing.Color.Orange;
             this.lblHeight.Location = new System.Drawing.Point(18, 54);
             this.lblHeight.Name = "lblHeight";
-            this.lblHeight.Size = new System.Drawing.Size(81, 26);
+            this.lblHeight.Size = new System.Drawing.Size(53, 18);
             this.lblHeight.TabIndex = 5;
             this.lblHeight.Text = "Height:";
             // 
@@ -175,7 +163,7 @@
             this.txtHeight.Enabled = false;
             this.txtHeight.Location = new System.Drawing.Point(227, 49);
             this.txtHeight.Name = "txtHeight";
-            this.txtHeight.Size = new System.Drawing.Size(100, 30);
+            this.txtHeight.Size = new System.Drawing.Size(100, 23);
             this.txtHeight.TabIndex = 4;
             // 
             // lblActivity
@@ -184,7 +172,7 @@
             this.lblActivity.BackColor = System.Drawing.Color.Orange;
             this.lblActivity.Location = new System.Drawing.Point(18, 80);
             this.lblActivity.Name = "lblActivity";
-            this.lblActivity.Size = new System.Drawing.Size(305, 26);
+            this.lblActivity.Size = new System.Drawing.Size(198, 18);
             this.lblActivity.TabIndex = 3;
             this.lblActivity.Text = "Usual Activity Level (in hours):";
             // 
@@ -193,7 +181,7 @@
             this.txtActLvl.Enabled = false;
             this.txtActLvl.Location = new System.Drawing.Point(227, 78);
             this.txtActLvl.Name = "txtActLvl";
-            this.txtActLvl.Size = new System.Drawing.Size(100, 30);
+            this.txtActLvl.Size = new System.Drawing.Size(100, 23);
             this.txtActLvl.TabIndex = 2;
             // 
             // lblName
@@ -202,7 +190,7 @@
             this.lblName.BackColor = System.Drawing.Color.Orange;
             this.lblName.Location = new System.Drawing.Point(18, 25);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(72, 26);
+            this.lblName.Size = new System.Drawing.Size(47, 18);
             this.lblName.TabIndex = 1;
             this.lblName.Text = "Name:";
             // 
@@ -211,7 +199,7 @@
             this.txtName.Enabled = false;
             this.txtName.Location = new System.Drawing.Point(227, 20);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(100, 30);
+            this.txtName.Size = new System.Drawing.Size(100, 23);
             this.txtName.TabIndex = 0;
             // 
             // tabActivity
@@ -225,10 +213,10 @@
             this.tabActivity.Controls.Add(this.combActivities);
             this.tabActivity.Controls.Add(this.btnClearActivity);
             this.tabActivity.Controls.Add(this.btnAddActivity);
-            this.tabActivity.Location = new System.Drawing.Point(4, 35);
+            this.tabActivity.Location = new System.Drawing.Point(4, 27);
             this.tabActivity.Name = "tabActivity";
             this.tabActivity.Padding = new System.Windows.Forms.Padding(3);
-            this.tabActivity.Size = new System.Drawing.Size(792, 572);
+            this.tabActivity.Size = new System.Drawing.Size(792, 580);
             this.tabActivity.TabIndex = 5;
             this.tabActivity.Text = "Activity";
             // 
@@ -237,7 +225,7 @@
             this.lblCaloriesBurnedDisplay.AutoSize = true;
             this.lblCaloriesBurnedDisplay.Location = new System.Drawing.Point(227, 261);
             this.lblCaloriesBurnedDisplay.Name = "lblCaloriesBurnedDisplay";
-            this.lblCaloriesBurnedDisplay.Size = new System.Drawing.Size(0, 26);
+            this.lblCaloriesBurnedDisplay.Size = new System.Drawing.Size(0, 18);
             this.lblCaloriesBurnedDisplay.TabIndex = 7;
             // 
             // lblCaloriesBurned
@@ -245,7 +233,7 @@
             this.lblCaloriesBurned.AutoSize = true;
             this.lblCaloriesBurned.Location = new System.Drawing.Point(32, 257);
             this.lblCaloriesBurned.Name = "lblCaloriesBurned";
-            this.lblCaloriesBurned.Size = new System.Drawing.Size(227, 26);
+            this.lblCaloriesBurned.Size = new System.Drawing.Size(148, 18);
             this.lblCaloriesBurned.TabIndex = 6;
             this.lblCaloriesBurned.Text = "Total Calories Burned: ";
             // 
@@ -253,7 +241,7 @@
             // 
             this.tbxDuration.Location = new System.Drawing.Point(183, 166);
             this.tbxDuration.Name = "tbxDuration";
-            this.tbxDuration.Size = new System.Drawing.Size(100, 30);
+            this.tbxDuration.Size = new System.Drawing.Size(100, 23);
             this.tbxDuration.TabIndex = 5;
             // 
             // lblSelectDuration
@@ -261,7 +249,7 @@
             this.lblSelectDuration.AutoSize = true;
             this.lblSelectDuration.Location = new System.Drawing.Point(32, 166);
             this.lblSelectDuration.Name = "lblSelectDuration";
-            this.lblSelectDuration.Size = new System.Drawing.Size(189, 26);
+            this.lblSelectDuration.Size = new System.Drawing.Size(124, 18);
             this.lblSelectDuration.TabIndex = 4;
             this.lblSelectDuration.Text = "Duration (minutes)";
             // 
@@ -270,7 +258,7 @@
             this.lblSelectActivity.AutoSize = true;
             this.lblSelectActivity.Location = new System.Drawing.Point(32, 120);
             this.lblSelectActivity.Name = "lblSelectActivity";
-            this.lblSelectActivity.Size = new System.Drawing.Size(149, 26);
+            this.lblSelectActivity.Size = new System.Drawing.Size(95, 18);
             this.lblSelectActivity.TabIndex = 3;
             this.lblSelectActivity.Text = "Select Activity";
             // 
@@ -286,7 +274,7 @@
             ""});
             this.combActivities.Location = new System.Drawing.Point(183, 120);
             this.combActivities.Name = "combActivities";
-            this.combActivities.Size = new System.Drawing.Size(121, 34);
+            this.combActivities.Size = new System.Drawing.Size(121, 26);
             this.combActivities.TabIndex = 2;
             // 
             // btnClearActivity
@@ -309,112 +297,6 @@
             this.btnAddActivity.UseVisualStyleBackColor = true;
             this.btnAddActivity.Click += new System.EventHandler(this.btnAddActivity_Click);
             // 
-            // SQLTest
-            // 
-            this.SQLTest.BackColor = System.Drawing.Color.Lime;
-            this.SQLTest.Controls.Add(this.btnViewFood);
-            this.SQLTest.Controls.Add(this.btnViewUser);
-            this.SQLTest.Controls.Add(this.btnInsertData);
-            this.SQLTest.Controls.Add(this.btnCreateTables);
-            this.SQLTest.Controls.Add(this.btnInsertTestData);
-            this.SQLTest.Controls.Add(this.txbResults);
-            this.SQLTest.Controls.Add(this.btnViewData);
-            this.SQLTest.Controls.Add(this.btnCreateTestTable);
-            this.SQLTest.Controls.Add(this.CreateConnectDb);
-            this.SQLTest.Location = new System.Drawing.Point(4, 35);
-            this.SQLTest.Name = "SQLTest";
-            this.SQLTest.Size = new System.Drawing.Size(792, 572);
-            this.SQLTest.TabIndex = 5;
-            this.SQLTest.Text = "SQLTest";
-            // 
-            // btnViewFood
-            // 
-            this.btnViewFood.Location = new System.Drawing.Point(15, 444);
-            this.btnViewFood.Name = "btnViewFood";
-            this.btnViewFood.Size = new System.Drawing.Size(110, 30);
-            this.btnViewFood.TabIndex = 2;
-            this.btnViewFood.Text = "View Food";
-            this.btnViewFood.UseVisualStyleBackColor = true;
-            this.btnViewFood.Click += new System.EventHandler(this.btnViewFood_Click);
-            // 
-            // btnViewUser
-            // 
-            this.btnViewUser.Location = new System.Drawing.Point(15, 403);
-            this.btnViewUser.Name = "btnViewUser";
-            this.btnViewUser.Size = new System.Drawing.Size(122, 23);
-            this.btnViewUser.TabIndex = 7;
-            this.btnViewUser.Text = "View User";
-            this.btnViewUser.UseVisualStyleBackColor = true;
-            this.btnViewUser.Click += new System.EventHandler(this.btnViewUser_Click);
-            // 
-            // btnInsertData
-            // 
-            this.btnInsertData.Location = new System.Drawing.Point(20, 346);
-            this.btnInsertData.Name = "btnInsertData";
-            this.btnInsertData.Size = new System.Drawing.Size(158, 36);
-            this.btnInsertData.TabIndex = 6;
-            this.btnInsertData.Text = "Insert testing data";
-            this.btnInsertData.UseVisualStyleBackColor = true;
-            this.btnInsertData.Click += new System.EventHandler(this.btnInsertData_Click);
-            // 
-            // btnCreateTables
-            // 
-            this.btnCreateTables.Location = new System.Drawing.Point(0, 297);
-            this.btnCreateTables.Name = "btnCreateTables";
-            this.btnCreateTables.Size = new System.Drawing.Size(125, 24);
-            this.btnCreateTables.TabIndex = 5;
-            this.btnCreateTables.Text = "Create Tables";
-            this.btnCreateTables.UseVisualStyleBackColor = true;
-            this.btnCreateTables.Click += new System.EventHandler(this.btnCreateTables_Click);
-            // 
-            // btnInsertTestData
-            // 
-            this.btnInsertTestData.Location = new System.Drawing.Point(3, 129);
-            this.btnInsertTestData.Name = "btnInsertTestData";
-            this.btnInsertTestData.Size = new System.Drawing.Size(149, 60);
-            this.btnInsertTestData.TabIndex = 4;
-            this.btnInsertTestData.Text = "Insert Test Data";
-            this.btnInsertTestData.UseVisualStyleBackColor = true;
-            this.btnInsertTestData.Click += new System.EventHandler(this.btnInsertTestData_Click);
-            // 
-            // txbResults
-            // 
-            this.txbResults.Location = new System.Drawing.Point(344, 95);
-            this.txbResults.Multiline = true;
-            this.txbResults.Name = "txbResults";
-            this.txbResults.Size = new System.Drawing.Size(393, 420);
-            this.txbResults.TabIndex = 3;
-            // 
-            // btnViewData
-            // 
-            this.btnViewData.Location = new System.Drawing.Point(3, 195);
-            this.btnViewData.Name = "btnViewData";
-            this.btnViewData.Size = new System.Drawing.Size(163, 40);
-            this.btnViewData.TabIndex = 2;
-            this.btnViewData.Text = "View Data";
-            this.btnViewData.UseVisualStyleBackColor = true;
-            this.btnViewData.Click += new System.EventHandler(this.btnViewData_Click);
-            // 
-            // btnCreateTestTable
-            // 
-            this.btnCreateTestTable.Location = new System.Drawing.Point(3, 92);
-            this.btnCreateTestTable.Name = "btnCreateTestTable";
-            this.btnCreateTestTable.Size = new System.Drawing.Size(184, 31);
-            this.btnCreateTestTable.TabIndex = 1;
-            this.btnCreateTestTable.Text = "Create Test Table";
-            this.btnCreateTestTable.UseVisualStyleBackColor = true;
-            this.btnCreateTestTable.Click += new System.EventHandler(this.btnCreateTestTable_Click);
-            // 
-            // CreateConnectDb
-            // 
-            this.CreateConnectDb.Location = new System.Drawing.Point(3, 52);
-            this.CreateConnectDb.Name = "CreateConnectDb";
-            this.CreateConnectDb.Size = new System.Drawing.Size(222, 34);
-            this.CreateConnectDb.TabIndex = 0;
-            this.CreateConnectDb.Text = "Create and collect Database";
-            this.CreateConnectDb.UseVisualStyleBackColor = true;
-            this.CreateConnectDb.Click += new System.EventHandler(this.CreateConnectDb_Click);
-            // 
             // btnClose
             // 
             this.btnClose.BackColor = System.Drawing.Color.RoyalBlue;
@@ -428,7 +310,7 @@
             // 
             // FitThisHUB
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
@@ -447,8 +329,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tabActivity.ResumeLayout(false);
             this.tabActivity.PerformLayout();
-            this.SQLTest.ResumeLayout(false);
-            this.SQLTest.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -470,16 +350,6 @@
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.TabPage tabDash;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TabPage SQLTest;
-        private System.Windows.Forms.Button CreateConnectDb;
-        private System.Windows.Forms.Button btnViewData;
-        private System.Windows.Forms.Button btnCreateTestTable;
-        private System.Windows.Forms.TextBox txbResults;
-        private System.Windows.Forms.Button btnInsertTestData;
-        private System.Windows.Forms.Button btnViewFood;
-        private System.Windows.Forms.Button btnViewUser;
-        private System.Windows.Forms.Button btnInsertData;
-        private System.Windows.Forms.Button btnCreateTables;
         private System.Windows.Forms.TabPage tabActivity;
         private System.Windows.Forms.Label lblSelectActivity;
         private System.Windows.Forms.Button btnClearActivity;
